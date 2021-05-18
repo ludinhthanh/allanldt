f20f2ad34b79c3b9785b1055278de4fdca671beb
ludinhthanh <57440677+ludinhthanh@users.noreply.github.com>
2021-05-18 10:07:24 +0700

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,26 +1,51 @@
-package binarysearch;
-public class Binarysearch {
-    int binarySearch(int arr[], int l, int r, int x) {
-        if (r >= l) {
-            int mid = l + (r - l) / 2;
-            if (arr[mid] == x)
-                return mid;
-            if (arr[mid] > x)
-                return binarySearch(arr, l, mid - 1, x);
-            return binarySearch(arr, mid + 1, r, x);
-        }        
-        return -1;
+package linearsearch;
+import java.util.*;
+public class LinearSearch {
+    public int[] array;
+public LinearSearch(){}
+public LinearSearch(int[] array){
+    this.array= array;
+}
+   @Override
+   public String toString(){
+       String s ="";
+       for(int a:array){
+           s=+a+"";
+                   }
+       return s;
+   }
+    public int[] getArray(){
+        return array;
     }
-
-    public static void main(String args[]) {
-        Binarysearch A = new Binarysearch();
-        int arr[] = { 1, 1, 1, 1, 3, 4, 6, 8, 9, 9, 10, 86 };
-        int n = arr.length;
-        int x =4 ;
-        int result = A.binarySearch(arr, 0, n - 1, x);
-        if (result == -1)
-            System.out.println("Phần tử không tồn tại.");
-        else
-            System.out.println("Phần tử được tìm thấy tại vị trí: " + result);
+    public void setArray(int [] array){
+        this.array=array;
     }
+    public int keyBoard(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of array: ");
+        int n=input.nextInt();
+        return n;
+    }
+    public void randomArray(int n) {
+        Random rd=new Random();
+        array = new int[n];
+        for(int i=0; i< array.length ; i++){
+            array[i]=rd.nextInt(10);
+        }
+    }
+    public int search(int x) {
+    for (int i = 0, length = array.length; i < length; i++) 
+            if (array[i] == x) return i;
+    return -1;
+    }
+    public static void main(String[] args) {
+        LinearSearch A = new LinearSearch();
+        Scanner sc = new Scanner(System.in);// nhập từ bàn phím
+        A.randomArray(A.keyBoard());
+        System.out.print("Enter search value: ");
+        int x = sc.nextInt();//khai báo để nhập từ bà phím
+        System.out.println("The array: " + Arrays.toString(A.array));
+        A.search(x);
+        System.out.println("Found " + x + " at index: " + (A.search(x)));
+    }  
 }
